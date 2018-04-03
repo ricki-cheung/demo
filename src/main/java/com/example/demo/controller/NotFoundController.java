ae87b36478318d02fbe4ead59f1f5aebfd26dc81
yanzhiying1 <1022175532@qq.com>
2018-04-03 10:51:37 +0800

    增加了访问错误url的controller

---
+++ src/main/java/com/example/demo/controller/NotFoundController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class NotFoundController implements ErrorController {
+	 @Override
+	    public String getErrorPath() {
+	        return "/error";
+	    }
+	    @RequestMapping(value = "/error")
+	    public Object error(HttpServletResponse response, HttpServletRequest request) {
+	       
+	       
+	        return new ModelAndView("404");
+
+	    }
+   
+}
