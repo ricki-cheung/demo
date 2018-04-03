95389dd96c0ea990329a50c48015f742734b328d
ricki <andyzzr@sina.com>
2018-04-03 16:02:05 +0800

    修改配置

--- src/main/java/com/example/demo/controller/NotFoundController.java
+++ src/main/java/com/example/demo/controller/NotFoundController.java
@@ -5,21 +5,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+//@RestController
 public class NotFoundController implements ErrorController {
-	 @Override
-	    public String getErrorPath() {
-	        return "/error";
-	    }
-	    @RequestMapping(value = "/error")
-	    public Object error(HttpServletResponse response, HttpServletRequest request) {
-	       
-	       
-	        return new ModelAndView("404");
+	@Override
+	public String getErrorPath() {
+		return "/error";
+	}
 
-	    }
-   
+	@RequestMapping(value = "/error")
+	public Object error(HttpServletResponse response, HttpServletRequest request) {
+
+		return new ModelAndView("404");
+
+	}
+
 }
