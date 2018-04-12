754f81cdbca41a482b418daf3992ab4e97fc6e58
andyzzr <andyzzr@sina.com>
2018-04-12 08:25:10 +0800

    添加logincontroller

---
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Ricki
+ *
+ */
+@Controller
+public class LoginController {
+	
+	
+
+}
