4800f0bf847f3b82c3c2e5475889c40aca609019
ricki <andyzzr@sina.com>
2018-04-13 10:34:52 +0800

    登录成功

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,15 +1,24 @@
+/**
+ * 
+ */
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * @author Ricki
+ *
+ */
 @Controller
-public class indexController {
-	
-	
+public class IndexController {
+
+	/**
+	 * @return
+	 * 登录成功后的首页页面
+	 */
 	@RequestMapping("/index")
 	public String indexPage() {
 		return "index";
 	}
-
 }
--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -3,11 +3,9 @@
  */
 package com.example.demo.controller;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.model.User;
 
@@ -20,26 +18,10 @@ public class LoginController {
 	
 	/**
 	 * @return
-	 * 处理用户登录操作
+	 * 登录页面
 	 */
-	@RequestMapping("/login")
-	public String login(User user) {
-		if(user == null) {
-			return "login";
-		}
-		
-		//创建一个用户
-		Subject userSubject = SecurityUtils.getSubject();
-		//定义一个token
-		UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(),user.getPassword());
-		//用户登录
-		try {
-			userSubject.login(token);
-		}catch(Exception e) {//TODO 这里可以细分异常类型，做出不同的处理
-			e.printStackTrace();
-			return "login";
-		}
-		
-		return "redirect:index";
+	@RequestMapping(path="/login",method=RequestMethod.GET)
+	public String loginPage(User user) {
+		return "login";
 	}
 }
