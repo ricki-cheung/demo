f27f0feaf04dc9e9a2d3331dc09455e9c615aa27
ricki <andyzzr@sina.com>
2018-04-16 21:23:01 +0800

    添加角色权限

--- src/main/java/com/example/demo/model/AuthRole.java
+++ src/main/java/com/example/demo/model/AuthRole.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.demo.model;
 
 /**
@@ -25,31 +24,3 @@ public class AuthRole {
 	
 	
 }
-=======
-package com.example.demo.model;
-
-/**
- * 权限_角色
- * 
- * @author Administrator
- *sys_role_auth表
- */
-public class AuthRole {
-	private Long roleId;
-	private String authId;
-	public Long getRoleId() {
-		return roleId;
-	}
-	public void setRoleId(Long roleId) {
-		this.roleId = roleId;
-	}
-	public String getAuthId() {
-		return authId;
-	}
-	public void setAuthId(String authId) {
-		this.authId = authId;
-	}
-	
-	
-}
->>>>>>> branch 'master' of https://github.com/ricki-cheung/demo.git
