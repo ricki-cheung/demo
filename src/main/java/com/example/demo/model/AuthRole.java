f4ad28ba6188f91a7f02a1fed21dcf71e2518967
ricki <andyzzr@sina.com>
2018-04-16 21:07:44 +0800

    恩恩

---
+++ src/main/java/com/example/demo/model/AuthRole.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+/**
+ * 权限_角色
+ * 
+ * @author Administrator
+ *sys_role_auth表
+ */
+public class AuthRole {
+	private Long roleId;
+	private String authId;
+	public Long getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+	public String getAuthId() {
+		return authId;
+	}
+	public void setAuthId(String authId) {
+		this.authId = authId;
+	}
+	
+	
+}
