dd3d26b60fdfb864c34677dc002999cc5c20da7d
yanzhiying1 <1022175532@qq.com>
2018-04-27 16:00:03 +0800

    新增getRoleListByAuthId方法

--- src/main/java/com/example/demo/mapper/RoleMapper.java
+++ src/main/java/com/example/demo/mapper/RoleMapper.java
@@ -1,5 +1,7 @@
 package com.example.demo.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -12,5 +14,6 @@ import com.example.demo.model.Role;
  */
 @Mapper
 public interface RoleMapper {
-	Role getRoleById(@Param("roleId") Integer roleId);
+	Role getRoleById(@Param(value="roleId") Integer roleId);
+	List<Role> getRoleListByAuthId(@Param(value="authId") String authId);
 }
