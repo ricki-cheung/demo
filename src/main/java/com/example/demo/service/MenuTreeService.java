7ae62af0db7147fd9837ca4a99caa2b3c8bbaa20
yanzhiying1 <1022175532@qq.com>
2018-04-28 14:41:10 +0800

    添加getUserByUserName方法

--- src/main/java/com/example/demo/service/MenuTreeService.java
+++ src/main/java/com/example/demo/service/MenuTreeService.java
@@ -13,6 +13,9 @@ import com.example.demo.model.Auth;
 import com.example.demo.model.Role;
 import com.example.demo.model.User;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 @Service
 public class MenuTreeService {
 	
@@ -99,25 +102,53 @@ public class MenuTreeService {
 	}
 	
 	
-	
 	/**
-	 * 查询用户的权限
+	 * 根据用户ID返回对应的JSON格式的菜单
+	 * @param id
+	 * @return
 	 */
-	/*public List<Auth> getAuthListByUserId(Integer userId) {
-		List<Role> roleList = getRoleListByUserId(userId);
-		Map<String,Auth> authMap = new HashMap<>();
-		List<Auth> authList = new ArrayList<>();
-		for (Role role : roleList) {
-			for (Auth auth : role.getAuthList()) {
-				if(authMap.get(auth.getAuthName()) == null) {
-					authMap.put(auth.getAuthName(), auth);
+	public JSONArray getMenu(Integer id) {
+		List<Auth> authList = this.getAuthByUserId(id);//查询权限
+		List<Auth> menuAuth = this.getMenuAuth(authList);//找出菜单权限
+		List<Auth> topMenuAuth = this.getTopMenuAuth(authList);//找出顶级菜单权限
+		Map<String, Auth> authMap = new HashMap<>();
+		JSONArray allJsonArray = new JSONArray();
+		JSONArray jsonArray = null;
+		
+		//循环把顶级菜单的父菜单传进去
+		for (Auth auth : topMenuAuth) {
+			//传进去之前应该判断之前是不是已经传过一样的父ID了,确保不会重复传同样的父ID进去
+			if(authMap.get(auth.getAuthParent()) == null) {
+				jsonArray = authMenu(menuAuth, auth.getAuthParent());
+				authMap.put(auth.getAuthParent(), auth);//map<父ID，auth>
+				if(jsonArray != null && jsonArray.size()!=0) {				
+					allJsonArray.addAll(jsonArray);
 				}
 			}
 		}
-		for (Map.Entry<String, Auth> entry : authMap.entrySet()) {
-			authList.add(entry.getValue());
+		
+		return allJsonArray;
+	}
+	
+	
+	
+	/**
+	 * 生成菜单树
+	 * @param authList
+	 * @param parentId  顶级菜单的父ID
+	 * @return
+	 */
+	private  JSONArray authMenu(List<Auth> authList,String parentId) {
+		JSONArray childMenu = new JSONArray();
+		for (Auth menu : authList) {
+			 if (parentId.equals(menu.getAuthParent())) {//从顶级菜单开始递归
+				 JSONObject jo = (JSONObject) JSONObject.fromObject(menu);
+				 JSONArray c_node = authMenu(authList, menu.getId());
+				 jo.put("children", c_node);
+				 childMenu.add(jo);
+			 }
 		}
-		return authList;
-	}*/
+		return childMenu;
+}
 	
 }
