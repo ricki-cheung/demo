243dd9d8111d78010458c6dec2665fd18ba41f00
yanzhiying1 <1022175532@qq.com>
2018-05-07 16:57:35 +0800

    新增方法

--- src/main/java/com/example/demo/service/PatientInfoService.java
+++ src/main/java/com/example/demo/service/PatientInfoService.java
@@ -3,6 +3,7 @@
  */
 package com.example.demo.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.mapper.PatientInfoMapper;
 import com.example.demo.model.PatientInfo;
+import com.example.demo.model.PatientQuery;
 
 /**
  * @author Ricki
@@ -38,6 +40,53 @@ public class PatientInfoService {
 	 */
 	public int getTotalCount(Map<String,Object> paramMap) {
 		return patientInfoMapper.getTotalCount(paramMap);
+	}
+	
+	/**
+	 * 根据查询条件查询病人列表
+	 * @param paramMap
+	 * @return
+	 */
+	public List<PatientInfo> queryPatientListByCondition(PatientQuery patientQuery){
+		Map<String,Object> paramMap = new HashMap<String,Object>();
+		//把patientQuery对象转为map
+		if(patientQuery.getCheckStatus() != 0) {
+			paramMap.put("checkStatus", patientQuery.getCheckStatus());
+		}
+		if(!"请选择".equals(patientQuery.getCareLevel().trim())) {
+			paramMap.put("careLevel", patientQuery.getCareLevel());
+		}
+		
+		if(!patientQuery.getPatientName().trim().equals("")) {
+			paramMap.put("patientName", "%"+patientQuery.getPatientName()+"%");
+		} 
+		if(!patientQuery.getInHospitalNo().trim().equals("")) {
+			paramMap.put("inHospitalNo", patientQuery.getInHospitalNo());
+		}
+		if(!patientQuery.getPatientType().trim().equals("")) {
+			paramMap.put("patientType", patientQuery.getPatientType());
+		}
+		if(!patientQuery.getPatientDoctor().trim().equals("")) {
+			paramMap.put("patientDoctor", patientQuery.getPatientDoctor());		
+		}
+		System.out.println(paramMap);
+		return patientInfoMapper.queryPatientListByCondition(paramMap);
+	}
+	
+	/**
+	 * 根据ID删除病人信息
+	 * @param id
+	 */
+	public void deletePatientById(Integer id) {
+		patientInfoMapper.deletePatientById(id);
+	}
+	
+	/**新增病人信息
+	 * 
+	 * @param paramMap
+	 */
+	public void addPitent(PatientInfo patientInfo) {
+		patientInfoMapper.addPitent(patientInfo);
 	}
 	
 
